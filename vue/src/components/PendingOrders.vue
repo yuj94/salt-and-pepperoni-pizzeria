782fbdd60f20f870a5ab5920dd95fce0aa662244
Andrew Pero <AndrewJPero@gmail.com>
2021-12-13 11:21:18 -0500

    baton pass 3

--- vue/src/components/PendingOrders.vue
+++ vue/src/components/PendingOrders.vue
@@ -17,32 +17,18 @@
       </thead>
       <tbody>
         <tr>
-          <td></td>
-          <td>
-            <input type="text" id="firstNameFilter" v-model="filter.firstName" />
-          </td>
-          <td>
-            <input type="text" id="lastNameFilter" v-model="filter.lastName" />
-          </td>
-          <td>
-            <input
-              type="text"
-              id="addressFilter"
-              v-model="filter.addressLine"
-            />
-          </td>
-          <td>
-            <input type="text" id="stateFilter" v-model="filter.addressState" />
-          </td>
-          <td>
-            <input type="text" id="cityFilter" v-model="filter.addressCity" />
-          </td>
-           <td></td>
-          <td><input type="text" id="EmailFilter" v-model="filter.email" /></td>
+          <td><input type="text" id="orderIdfilter" v-model="filter.orderId"/></td>
+          <td><input type="text" id="firstNameFilter" v-model="filter.firstName"/></td>
+          <td><input type="text" id="lastNameFilter" v-model="filter.lastName"/></td>
+          <td><input type="text" id="addressFilter" v-model="filter.addressLine"/></td>
+          <td><input type="text" id="stateFilter" v-model="filter.addressState"/></td>
+          <td><input type="text" id="cityFilter" v-model="filter.addressCity"/></td>
+          <td><input type="text" id="zipCodeFilter" v-model="filter.addressZipCode"/></td>
+          <td><input type="text" id="EmailFilter" v-model="filter.email"/></td>
           <td><input type="text" id="phoneNumberFilter" v-model="filter.phoneNumber"/></td>
-           <td></td>
+          <td><input type="text" id="orderTotalFilter" v-model="filter.orderTotal"/></td>
         </tr>
-       <tr v-for="order in filteredList" v-bind:key="order.orderId">
+        <tr v-for="order in filteredList" v-bind:key="order.orderId">
           <td>{{ order.orderId }}</td>
           <td>{{ order.firstName }}</td>
           <td>{{ order.lastName }}</td>
@@ -53,14 +39,14 @@
           <td>{{ order.email }}</td>
           <td>{{ order.phoneNumber }}</td>
           <td>{{ order.orderTotal }}</td>
-        </tr> 
+        </tr>
       </tbody>
     </table>
   </div>
 </template>
 
 <script>
-import orderService from '@/services/OrderService.js';
+import orderService from "@/services/OrderService.js";
 
 export default {
   name: "pending-orders-list",
@@ -83,17 +69,22 @@ export default {
   computed: {
     filteredList() {
       return this.$store.state.pendingOrders.filter((order) => {
-        return order.firstName.toLowerCase().includes(this.filter.firstName.toLowerCase()) &&
+        return (
+          order.orderId.toString().includes(this.filter.orderId) &&
+          order.firstName.toLowerCase().includes(this.filter.firstName.toLowerCase()) &&
           order.lastName.toLowerCase().includes(this.filter.lastName.toLowerCase()) &&
           order.addressLine.toLowerCase().includes(this.filter.addressLine.toLowerCase()) &&
           order.addressState.toLowerCase().includes(this.filter.addressState.toLowerCase()) &&
           order.addressCity.toLowerCase().includes(this.filter.addressCity.toLowerCase()) &&
           order.email.toLowerCase().includes(this.filter.email.toLowerCase()) &&
-          order.phoneNumber.toLowerCase().includes(this.filter.phoneNumber.toLowerCase()); 
+          order.addressZipCode.toString().includes(this.filter.addressZipCode) &&
+          order.phoneNumber.toLowerCase().includes(this.filter.phoneNumber.toLowerCase()) &&
+          order.orderTotal.toString().includes(this.filter.orderTotal)
+        );
       });
     },
   },
- methods: {
+  methods: {
     getPendingOrders() {
       orderService.getAllPendingOrders().then((response) => {
         this.$store.commit("SET_PENDING_ORDERS", response.data);
